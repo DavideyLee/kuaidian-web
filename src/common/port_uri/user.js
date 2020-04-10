7b49d3e619164df8125821803af37f707d84b908
李万伟 <liwanwei@zto.com>
2020-04-10 13:57:10 +0800

    前后端分离部署

--- src/common/port_uri/user.js
+++ src/common/port_uri/user.js
@@ -10,15 +10,15 @@
  */
 
 //获取用户信息
-exports.info = process.env.API_URL+"/api/get/user/info"
+exports.info = process.env.API_URL + "/api/get/user/info"
 //用户登录
-exports.login = process.env.API_URL+"/login"
+exports.login = process.env.API_URL + "/api/login"
 //用户登出
-exports.logout = process.env.API_URL+"/logout"
+exports.logout = process.env.API_URL + "/api/logout"
 //修改用户密码
-exports.changepasswd = process.env.API_URL+"/changePasswd"
-    //用户注册
-exports.register = process.env.API_URL+"/register"
+exports.changepasswd = process.env.API_URL + "/api/changePasswd"
+//用户注册
+exports.register = process.env.API_URL + "/api/register"
 
-exports.users = process.env.API_URL+"/api/get/user"
-exports.usersProject = process.env.API_URL+"/api/get/user/project"
+exports.users = process.env.API_URL + "/api/get/user"
+exports.usersProject = process.env.API_URL + "/api/get/user/project"
